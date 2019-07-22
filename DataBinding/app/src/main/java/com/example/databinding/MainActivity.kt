7c428f81526c1213b3dd824bf98ed6c2c4946493
sagerin <rin.sagehashi@moi.st>
2019-07-22 17:47:58 +0900

    onCreateで変更してみた

--- DataBinding/app/src/main/java/com/example/databinding/MainActivity.kt
+++ DataBinding/app/src/main/java/com/example/databinding/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.databinding
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.name = "sagerin"
     }
 }
