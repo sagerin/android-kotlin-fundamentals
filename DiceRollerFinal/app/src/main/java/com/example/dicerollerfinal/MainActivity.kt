b57c568dfc3cbe3e1814fa49dc9f171463b3b2eb
sagerin <rin.sagehashi@moi.st>
2019-07-22 16:40:54 +0900

    ダイスロールするボタンを作成

--- DiceRollerFinal/app/src/main/java/com/example/dicerollerfinal/MainActivity.kt
+++ DiceRollerFinal/app/src/main/java/com/example/dicerollerfinal/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.dicerollerfinal
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var resultText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.rollButton)
+        rollButton.setOnClickListener { rollDice() }
+
+        resultText = findViewById(R.id.textView)
+        resultText.text = "Dice Rolled!"
+    }
+
+    private fun rollDice() {
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        val randomInt = Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
     }
 }
